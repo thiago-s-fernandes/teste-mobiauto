9ed042a54669cac05a375ab007a2e96a6648a649
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-02-28 16:15:11 -0300

    chore: add site base url in ssr

--- tabela-fipe/next.config.ts
+++ tabela-fipe/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    SITE_BASE_URL: process.env.SITE_BASE_URL
+  }
 };
 
 export default nextConfig;
