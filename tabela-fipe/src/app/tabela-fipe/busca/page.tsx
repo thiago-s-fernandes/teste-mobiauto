419df8b2bc8eee7129597c557162647bcbd8af01
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-02-28 14:53:54 -0300

    feat: add page busca

---
+++ tabela-fipe/src/app/tabela-fipe/busca/page.tsx
@@ -0,0 +1,51 @@
+import Form from "@/components/form/form";
+import Header from "@/components/header/header";
+import { FipeBrands } from "@/types/fipe";
+import { Box, Container } from "@mui/material";
+import { BASE_URL } from "@/constants";
+
+async function fetchBrands(): Promise<FipeBrands[]> {
+  try {
+    const res = await fetch(`${BASE_URL}/api/tabela-fipe/brands`);
+
+    if (!res.ok) {
+      throw new Error("Erro ao buscar marcas");
+    }
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error({ error });
+    return [];
+  }
+}
+
+export default async function Busca() {
+  const brands = await fetchBrands();
+
+  return (
+    <Box component="main" role="main" sx={{ height: "100%" }}>
+      <Box component="section" sx={{ height: "100%" }}>
+        <Container sx={{ height: "100%" }} component="article">
+          <Box
+            sx={{
+              display: "flex",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              gap: "16px"
+            }}
+          >
+            <Header
+              title="Tabela Fipe"
+              subtitle="Consulte o valor de um veículo de forma gratuita"
+            />
+            <Form brands={brands} />
+          </Box>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
