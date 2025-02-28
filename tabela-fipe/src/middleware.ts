d3fa7112fbbdbec7558530cf016411ce28e6feee
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-02-28 10:56:58 -0300

    feat: add middleware and redirect / for /tabela-fipe/busca

---
+++ tabela-fipe/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/tabela-fipe/busca", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
+  ]
+};
