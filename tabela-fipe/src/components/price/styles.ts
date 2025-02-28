134324231aeeb3076026a64dfd2334345e6e047d
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-02-28 14:55:12 -0300

    feat: style price component

---
+++ tabela-fipe/src/components/price/styles.ts
@@ -0,0 +1,18 @@
+import { styled } from "@mui/material/styles";
+
+export const PriceContainer = styled("div")`
+  align-items: center;
+  background-color: #00a38c;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  padding: 8px 24px;
+  width: 100%;
+`;
+
+export const PriceText = styled("p")`
+  color: #ffffff;
+  font-size: 34px;
+  font-weight: 700;
+  line-height: 42px;
+`;
