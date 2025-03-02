ad0056e3bf36c720ccb60910c6267e26db42283c
Thiago Fernandes <thiago.souzax3@outlook.com>
2025-03-02 05:51:27 -0300

    feat: add cache in middleware for apis

--- tabela-fipe/src/middleware.ts
+++ tabela-fipe/src/middleware.ts
@@ -1,9 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+// Middleware para cache e redirecionamento
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/tabela-fipe/busca", request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith("/api/")) {
+    const response = NextResponse.next();
+
+    response.headers.set(
+      "Cache-Control",
+      "public, max-age=300, stale-while-revalidate=59"
+    );
+    return response;
   }
 
   return NextResponse.next();
